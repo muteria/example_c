1992688f5fa3bbd9742f78af9d2724d8e666eaa7
Titcheu Chekam Thierry <thierry-tct@users.noreply.github.com>
2020-12-22 16:38:17 +0100

    Update conf_shadow_semu.py

--- ctrl/conf_shadow_semu.py
+++ ctrl/conf_shadow_semu.py
@@ -33,20 +33,20 @@ shadow_se_test.set_one_test_execution_timeout(2)
 
 semu_test = TestcaseToolsConfig(tooltype=TestToolType.USE_CODE_AND_TESTS, toolname='semu', \
                         tool_user_custom=ToolUserCustom(
-                            PRE_TARGET_CMD_ORDERED_FLAGS_LIST=[
-                                #('-semu-disable-statediff-in-testgen',),
-                                #('-semu-continue-mindist-out-heuristic',),
+                            PRE_TARGET_CMD_ORDERED_FLAGS_LIST=[                              
+                                #('-semu-no-state-difference',),
+                                #('-semu-MDO-propagation-selection-strategy',),
                                 #('-semu-use-basicblock-for-distance',),
                                 ('-semu-forkprocessfor-segv-externalcalls',),
                                 #('-semu-testsgen-only-for-critical-diffs',),
-                                #('-semu-consider-outenv-for-diffs',),
+                                #('-semu-no-environment-output-diff',),
 
-                                ('-semu-mutant-max-fork', '0'),
-                                ('-semu-checknum-before-testgen-for-discarded', '2'),
-                                ('-semu-mutant-state-continue-proba', '0.25'),
+                                ('-semu-checkpoint-window', '0'),
+                                ('-semu-minimum-propagation-depth', '2'),
+                                ('-semu-propagation-proportion', '0.25'),
                                 ('-semu-precondition-length', '0'), # start from top
                                 #('-semu-max-total-tests-gen', '1000')
-                                ('-semu-max-tests-gen-per-mutant', '5'),
+                                ('-semu-number-of-tests-per-mutant', '5'),
                             ], 
                             POST_TARGET_CMD_ORDERED_FLAGS_LIST=[('-sym-args', '2', '2', '2')]
                             )
